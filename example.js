14c5df1bdf978ce5c8aaea5a5e4a4a29087d0082
noffle <sww@eight45.net>
2018-05-13 11:22:10 -0700

    implementation

---
+++ example.js
@@ -0,0 +1,33 @@
+var kappa = require('.')
+var hypercore = require('hypercore')
+
+var core = kappa('./log', { valueEncoding: 'json' })
+
+var sum = 0
+
+var sumview = {
+  api: {
+    get: function (cb) {
+      this.ready(function () {
+        cb(null, sum)
+      })
+    }
+  },
+  map: function (msgs, next) {
+    msgs.forEach(function (msg) {
+      if (typeof msg.value === 'number') sum += msg.value
+    })
+    next()
+  }
+}
+
+// the api will be mounted at core.api.sum
+core.use('sum', sumview)
+
+core.feed('default', function (err, feed) {
+  feed.append(1, function (err) {
+    core.api.sum.get(function (err, value) {
+      console.log(value) // 1
+    })
+  })
+})
---
+++ index.js
@@ -0,0 +1,31 @@
+var hypercore = require('hypercore')
+var multifeed = require('multifeed')
+var indexer = require('multifeed-index')
+
+module.exports = Kappa
+
+function Kappa (storage, opts) {
+  if (!(this instanceof Kappa)) return new Kappa(storage, opts)
+
+  this._logs = multifeed(hypercore, storage, opts)
+  this._indexes = {}
+
+  this.api = {}
+}
+
+// TODO: support versions + checking to rebuild them
+Kappa.prototype.use = function (name, view) {
+  this._indexes[name] = indexer({
+    log: this._logs,
+    maxBatch: 10,
+    batch: view.map
+  })
+  this.api[name] = {}
+  for (var key in view.api) {
+    this.api[name][key] = view.api[key].bind(this._indexes[name])
+  }
+}
+
+Kappa.prototype.feed = function (name, cb) {
+  this._logs.writer(name, cb)
+}
